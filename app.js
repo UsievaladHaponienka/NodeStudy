3adbc4af87b1a691013291edc1bfd8528af36f3b
Usievalad Haponienka <usievalad.haponienka@gmail.com>
2023-02-23 15:37:15 +0500

    Node Study
     - Created dummy object which extends EventEmitter. Assigned event and emitted it.

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
-const tutorial = require('./tutorial');
-console.log(tutorial.sum(2, 3));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+// eventEmitter.on('tutorial', function (num1, num2) {
+//     console.log(num1 + num2)
+// })
+//
+// eventEmitter.emit('tutorial', 1, 2);
+
+
+class Person extends EventEmitter {
+    constructor(name) {
+        super();
+        this._name = name
+    }
+
+    get name() {
+        return this._name
+    }
+}
+
+//Create new object instance
+let pedro = new Person('Pedro');
+let christina = new Person('Christina');
+
+//Assign event with name `name`
+pedro.on('name', function () {
+    console.log('My name is ' + pedro.name)
+})
+
+christina.on('name', function () {
+    console.log('My name is ' + christina.name)
+})
+
+//Emit `name` event
+pedro.emit('name');
+christina.emit('name');
+
